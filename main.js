9440815f45c4c82057d8c09cafdc9e1945020501
Athena112233 <43796329+Athena112233@users.noreply.github.com>
2020-06-23 13:24:54 -0700

    add event listener

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+function init(){
+    var darkplanet1 = document.getElementById('darkplanet1');
+    var darkplanet2 = document.getElementById('darkplanet2');
+    var darkplanet3 = document.getElementById('darkplanet3');
+    var darkplanet4 = document.getElementById('darkplanet4');
+    var brightplanet1 = document.getElementById('brightplanet1');
+    var brightplanet2 = document.getElementById('brightplanet2');
+    var brightplanet3 = document.getElementById('brightplanet3');
+    
+    darkplanet1.addEventListener("click", displayCard);
+    darkplanet2.addEventListener('click', displayCard);
+    darkplanet3.addEventListener('click', displayCard);
+    darkplanet4.addEventListener('click', displayCard);
+    brightplanet1.addEventListener('click', displayCard);
+    brightplanet2.addEventListener('click', displayCard);
+    brightplanet3.addEventListener('click', displayCard);
+
+    darkplanet1.addEventListener('touchend', displayCard);
+    darkplanet2.addEventListener('touchend', displayCard);
+    darkplanet3.addEventListener('touchend', displayCard);
+    darkplanet4.addEventListener('touchend', displayCard);
+    brightplanet1.addEventListener('touchend', displayCard);
+    brightplanet2.addEventListener('touchend', displayCard);
+    brightplanet3.addEventListener('touchend', displayCard);
+}
+
 function displayCard(){
-    console.log('Hello World')
+    console.log('Hello World');
 }
